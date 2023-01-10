00564a6ae102768422fb9ec839c2407a1f3a79b9
Rajesh kumar halder <82487522+Rajesh144142@users.noreply.github.com>
2023-01-10 09:17:28 +0530

    first-commit

---
+++ home.js
@@ -0,0 +1,61 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
+
+}
+function myFunctionone() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("More");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunctiontwo() {
+    var dots = document.getElementById("dotsOne");
+    var moreText = document.getElementById("MoreOne");
+    var btnText = document.getElementById("myBtnOne");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunctionthree() {
+    var dots = document.getElementById("dotstwo");
+    var moreText = document.getElementById("Moretwo");
+    var btnText = document.getElementById("myBtntwo");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
